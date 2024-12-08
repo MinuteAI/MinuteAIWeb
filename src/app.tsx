469e8798a4001bab0b07081792be3bb9c1e39241
Kai <kaizhengde@gmail.com>
2024-12-08 12:51:10 +0100

    web: routing issues fix

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,7 @@ import GencastTerms from './app/gencast/gencast-terms';
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gencast" element={<Home />} />
